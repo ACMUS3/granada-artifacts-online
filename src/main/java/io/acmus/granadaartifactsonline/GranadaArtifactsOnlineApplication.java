61ec586af0c60940f32799ea03831c73be33ca38
Halim <33067128+ACMUS3@users.noreply.github.com>
2024-02-20 21:46:00 +0200

    This is the first commit.

---
+++ src/main/java/io/acmus/granadaartifactsonline/GranadaArtifactsOnlineApplication.java
@@ -0,0 +1,13 @@
+package io.acmus.granadaartifactsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranadaArtifactsOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GranadaArtifactsOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/acmus/granadaartifactsonline/GranadaArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.acmus.granadaartifactsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranadaArtifactsOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
